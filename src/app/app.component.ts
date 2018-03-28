db1129415ab1d2e7971b7325af122f35935d3b81
Erick Oliva <Erick.Oliva@synechron.com>
2018-03-28 17:01:25 -0400

    Translate prove of concept

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+import {TranslateService} from 'ng2-translate';
+
+
+
 
 @Component({
   selector: 'app-root',
@@ -6,5 +10,27 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+
+  title = 'Translate Prove of Concept';
+  param = {value: 'world'};
+
+
+  // constructor(translate: TranslateService) {
+  //     // this language will be used as a fallback when a translation isn't found in the current language
+  //     translate.setDefaultLang('en');
+
+  //      // the lang to use, if the lang isn't available, it will use the current loader to get them
+  //     translate.use('en');
+
+
+      constructor(private translate: TranslateService) {
+        translate.addLangs(["en", "es"]);
+        translate.setDefaultLang('en');
+
+        let browserLang = translate.getBrowserLang();
+        translate.use(browserLang.match(/en|es/) ? browserLang : 'en');
+    }
+  }
 }
+
+
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,30 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
-
 import { AppComponent } from './app.component';
+
+import {HttpModule, Http} from "@angular/http";
+import {TranslateModule, TranslateStaticLoader,TranslateLoader} from "ng2-translate";
+
+
+export function createTranslateLoader(http: Http) {
+  return new TranslateStaticLoader(http, './assets/i18n', '.json');
+}
 
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
   imports: [
-    BrowserModule
+      BrowserModule,
+      HttpModule,
+      TranslateModule.forRoot({
+        provide: TranslateLoader,
+        useFactory: (createTranslateLoader),
+        deps: [Http]
+    })
   ],
-  providers: [],
+  declarations: [AppComponent],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+
+
+export class AppModule {
+}
